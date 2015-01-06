15d9ac0952c9fa7d616796fa914f8dd24eab010c
Viktor Sadovnikov <viktor@jv-ration.com>
2015-01-06 17:03:23 +0100

    naming agent, waiting for build server to come up

--- setup-agent.sh
+++ setup-agent.sh
@@ -7,12 +7,18 @@ if [ ! -d "$AGENT_DIR" ]; then
     echo "Setting up TeamCityagent for the first time..."
     echo "Agent will be installed to ${AGENT_DIR}."
     mkdir -p $AGENT_DIR
-    wget $TEAMCITY_SERVER/update/buildAgent.zip
+
+    until $(wget $TEAMCITY_SERVER/update/buildAgent.zip); do
+        echo "Waiting for '$TEAMCITY_SERVER' server..."
+        sleep 10
+    done
+
     unzip -q -d $AGENT_DIR buildAgent.zip
     rm buildAgent.zip
     chmod +x $AGENT_DIR/bin/agent.sh
 	
     echo "serverUrl=${TEAMCITY_SERVER}" > $AGENT_DIR/conf/buildAgent.properties
+    echo "name=${HOSTNAME}" >> $AGENT_DIR/conf/buildAgent.properties
     echo "workDir=../work" >> $AGENT_DIR/conf/buildAgent.properties
     echo "tempDir=../temp" >> $AGENT_DIR/conf/buildAgent.properties
     echo "systemDir=../system" >> $AGENT_DIR/conf/buildAgent.properties
